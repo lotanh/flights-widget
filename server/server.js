eebad1d1df3f6444b73e1f4154e82f2b91b8e162
Lotan Hadad <lotan3600@gmail.com>
2022-08-31 22:20:29 +0300

    Build the server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const cors = require("cors");
-const fetch = require('fetch')
 const app = express();
-
-
+const fetch = require("node-fetch")
+require('dotenv').config()
 app.use(express.json());
 app.use(cors());
+
+
+app.get('/' , (req,res) => {
+    res.send('Server app')
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
